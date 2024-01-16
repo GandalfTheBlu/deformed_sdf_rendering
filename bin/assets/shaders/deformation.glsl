b609abd916b07c54ecfa4adf6ee75af989be0baf
GandalfTheBlu <gandalflindkvist@gmail.com>
2024-01-16 21:56:29 +0100

    worked on linear blend deform

--- bin/assets/shaders/deformation.glsl
+++ bin/assets/shaders/deformation.glsl
@@ -13,6 +13,17 @@ vec3 Kelvinlet(vec3 point, vec3 center, vec3 force)
 
 vec3 LinearBlend(vec3 point)
 {	
+	const vec3 undefBonePoints[2] = vec3[](
+		vec3(0.),
+		vec3(0., 3., 0.)
+	);
+	
+	// x: radius of effect, y: falloff slope (positive)
+	const vec2 boneLinearFalloffs[2] = vec2[](
+		vec2(4., 2.),
+		vec2(8., 1.5)
+	);
+
 	const mat4 inverseUndefBoneTransforms[2] = mat4[](
 		mat4(
 			1., 0., 0., 0.,
@@ -24,42 +35,45 @@ vec3 LinearBlend(vec3 point)
 			1., 0., 0., 0.,
 			0., 1., 0., 0.,
 			0., 0., 1., 0.,
-			0., -4., 0., 1.
+			0., -8., 0., 1.
 		)
 	);
 	
+	const float angle = u_kelvinletSharpness / 16. * 3.14159;
+	const float cs = cos(angle);
+	const float sn = sin(angle);
 	const mat4 defBoneTransforms[2] = mat4[](
-		mat4(
-			1., 0., 0., 0.,
-			0., 2., 0., 0.,
-			0., 0., 1., 0.,
-			0., 0., 0., 1.
-		),
 		mat4(
 			1., 0., 0., 0.,
 			0., 1., 0., 0.,
 			0., 0., 1., 0.,
-			2., 4., 0., 1.
+			1.5, 0., 0., 1.
+		),
+		mat4(
+			cs, 0., -sn, 0.,
+			0., 1., 0., 0.,
+			sn, 0., cs, 0.,
+			0., 8., 0., 1.
 		)
 	);
 	
-	vec3 result = vec3(0.);
+	vec3 displacement = vec3(0.);
 	
 	for(int i=0; i<2; i++)
-	{
-		// use distance falloff function some way instead
-		float boneWeight = 0.5;
+	{	
+		vec3 diff = point - undefBonePoints[i];
+		float dist = sqrt(dot(diff, diff));
+		vec2 falloff = boneLinearFalloffs[i];
+		float boneWeight = max(0., falloff.x - dist * falloff.y) / falloff.x;
 		
-		// point relative to bone's default transform
-		vec4 relPoint = inverseUndefBoneTransforms[i] * vec4(point, 1.);
+		// these matrices should be premultiplied on cpu
+		vec4 defPoint = defBoneTransforms[i] * inverseUndefBoneTransforms[i] * vec4(point, 1.);
 		
-		// point in world space, deformed by bone
-		vec4 defPoint = defBoneTransforms[i] * relPoint;
 		
-		result += boneWeight * defPoint.xyz;
+		displacement += boneWeight * (defPoint.xyz - point);
 	}
 	
-	return result;
+	return point + displacement;
 }
 
 vec3 Deform(vec3 pos)
