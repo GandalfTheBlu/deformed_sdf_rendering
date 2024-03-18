91f05ce610c2f3b7538b4343ef69eda624cead89
GandalfTheBlu <gandalflindkvist@gmail.com>
2024-03-18 14:57:06 +0100

    Update app.cc

--- projects/setup_test/app.cc
+++ projects/setup_test/app.cc
@@ -890,7 +890,7 @@ void App_SetupTest::Init()
 void App_SetupTest::UpdateLoop()
 {
 	Engine::FileWatcher sdfWatcher;
-	sdfWatcher.Init("assets/shaders/deform_frag.glsl");
+	sdfWatcher.Init("assets/shaders/sdf.glsl");
 
 	double time0 = 0.;
 
